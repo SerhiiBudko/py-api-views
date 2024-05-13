a3f46bd6f7d979b81ef28b8bc93e2b8c14fb88e6
SerhiiBudko <140949363+SerhiiBudko@users.noreply.github.com>
2024-05-13 20:15:53 +0200

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -16,11 +16,10 @@ routers.register("cinema_halls", CinemaHallViewSet)
 routers.register("movies", MovieViewSet)
 
 urlpatterns = [
-    path("", include(routers.urls)),
     path("actors/", ActorList.as_view(), name="actors_list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actors_detail"),
     path("genres/", GenreList.as_view(), name="genres_list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genres_detail")
-]
+] + routers.urls
 
 app_name = "cinema"
